1ab0b48bdecb9c958cb67f35817c90386e51fde2
Rodrigo-EtecProfMariaCristinaMedeiros <80193792+RodrigoAntonio321@users.noreply.github.com>
2022-05-02 11:00:41 -0300

    Add files via upload

---
+++ aula 02_05/exercicio-01.js
@@ -0,0 +1,8 @@
+var let = (prompt = ('Digite uma letra'))
+
+if (let == 'A' || let == 'E'|| let == 'I' || let == 'O' || let == 'U') {
+ document.write = ('Sua letra é uma vocal')
+}
+else{
+    document.write = ('Sua letra é uma consoante')
+}
